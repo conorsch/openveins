aa578cbb0f8d9f6e4806e7b549541699261c186d
Conor Schaefer <conor.schaefer@gmail.com>
2015-02-07 13:10:28 -0500

    slightly better parser

--- parse.py
+++ parse.py
@@ -1,16 +1,49 @@
-#!/usr/bin/env python3
+##!/usr/bin/env python3
+# adapted from http://stackoverflow.com/questions/5036605/how-to-export-a-mysql-database-to-json
 import json
-import codecs
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 
-filename = "wp_posts.json"
+Base = declarative_base()
+SOUGHT_ATTRIBUTES = "post_date post_date_gmt post_content post_title ID".split()
 
-f = codecs.open(filename, 'r', 'utf-8')
-s = f.read()
-s = s.replace('\r', '\\r').replace('\n', '\\n')
-s = s.replace('\r', '\\r').replace('\n', '\\n')
+class WordpressPost(object):
 
-print("Char 656 is: '{}'".format(s[656]))
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs:
+            setattr(self, k, v)
+
+    def make_dict(self):
+        return {attrib: getattr(self, attrib) for attrib in SOUGHT_ATTRIBUTES}
 
 
-json.loads(s)
+import sqlalchemy
+metadata = sqlalchemy.MetaData()
+wp_posts_table = sqlalchemy.Table('wp_posts', metadata,
+        sqlalchemy.Column('ID', sqlalchemy.Integer, primary_key=True),
+        sqlalchemy.Column('post_title', sqlalchemy.String(100)),
+        sqlalchemy.Column('post_content', sqlalchemy.String(100)),
+        sqlalchemy.Column('post_date', sqlalchemy.Date),
+        sqlalchemy.Column('post_date_gmt', sqlalchemy.Date),
+    )
+
+# connect the database.  substitute the needed values.
+engine = sqlalchemy.create_engine('mysql://vagrant:vagrant@localhost/ronocdh_openveins?charset=utf8')
+
+# if needed, create the table:
+metadata.create_all(engine)
+
+# map the model to the table
+import sqlalchemy.orm
+sqlalchemy.orm.mapper(WordpressPost, wp_posts_table)
+
+QUERY = 'SELECT * FROM wp_posts;'
+wp_posts = list(engine.execute(QUERY))
+
+
+# and lets make some json out of it:
+import json
+
+wp_posts_json = json.dumps(wp_posts)
