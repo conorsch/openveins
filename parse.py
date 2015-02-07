c084eb2d2b930aa7c8401a39763da5b226388ad1
Conor Schaefer <conor.schaefer@gmail.com>
2015-02-07 13:20:10 -0500

    super terse parser

--- parse.py
+++ parse.py
@@ -3,50 +3,17 @@
 import json
 import sqlalchemy
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-
-
-Base = declarative_base()
-SOUGHT_ATTRIBUTES = "post_date post_date_gmt post_content post_title ID".split()
-
-class WordpressPost(object):
-
-    def __init__(self, *args, **kwargs):
-        for k, v in kwargs:
-            setattr(self, k, v)
-
-    def make_dict(self):
-        return {attrib: getattr(self, attrib) for attrib in SOUGHT_ATTRIBUTES}
-
-
-metadata = sqlalchemy.MetaData()
-wp_posts_table = sqlalchemy.Table('wp_posts', metadata,
-        sqlalchemy.Column('ID', sqlalchemy.Integer, primary_key=True),
-        sqlalchemy.Column('post_title', sqlalchemy.String(100)),
-        sqlalchemy.Column('post_content', sqlalchemy.String(100)),
-        sqlalchemy.Column('post_date', sqlalchemy.String(100)),
-        sqlalchemy.Column('post_date_gmt', sqlalchemy.String(100)),
-    )
-
 # connect the database.  substitute the needed values.
 engine = sqlalchemy.create_engine('mysql://vagrant:vagrant@localhost/ronocdh_openveins?charset=utf8')
-
-# if needed, create the table:
-metadata.create_all(engine)
-
-# map the model to the table
-import sqlalchemy.orm
-sqlalchemy.orm.mapper(WordpressPost, wp_posts_table)
 
 QUERY = 'SELECT ID, post_title, post_content FROM wp_posts;'
 wp_posts = list(engine.execute(QUERY))
 wp_posts = [dict(p) for p in wp_posts]
 
 
-# and lets make some json out of it:
-import json
-
 wp_posts_json = json.dumps(wp_posts)
 with open('wp_posts.json', 'w') as f:
     f.write(wp_posts_json)
+
+j = json.load(open('wp_posts.json'))
+print(j)
