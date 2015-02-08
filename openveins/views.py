a094e19840eae3bbf175a2a9ddd679671c915ca5
Conor Schaefer <conor.schaefer@gmail.com>
2015-02-07 23:10:16 -0500

    adds footer

--- openveins/views.py
+++ openveins/views.py
@@ -4,6 +4,7 @@ from openveins.models import Quote
 
 def home(request):
     quotes = Quote.objects.all()
+#    quotes = quotes[0:2]
     return render_to_response(
         'index.html',
         {'quotes': quotes},
