6ed86b4094c24f6bd54f0f3e99b01887e9a4e076
Conor Schaefer <conor.schaefer@gmail.com>
2015-02-08 01:18:25 -0500

    adds quote text field

--- openveins/search_indexes.py
+++ openveins/search_indexes.py
@@ -6,6 +6,7 @@ from openveins.models import Quote
 class QuoteIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     author = indexes.CharField(model_attr='user')
+    quote_text = indexes.CharField(model_attr='text')
     post_date = indexes.DateTimeField(model_attr='post_date')
 
     def get_model(self):
