502fdc72b84a1deb767cbe7f2d640b2c814a9713
Conor Schaefer <conor.schaefer@gmail.com>
2015-02-07 23:34:38 -0500

    corny pagination

--- openveins/views.py
+++ openveins/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from openveins.models import Quote
+from endless_pagination.decorators import page_template
 
 
-def home(request):
+@page_template('index.html')
+def home(
+        request,
+        template='index.html',
+        extra_context=None):
+    context = {
+        'quotes': Quote.objects.all(),
+    }
+    if extra_context:
+        context.update(extra_context)
+    return render_to_response(
+        template, context, context_instance=RequestContext(request))
+
+def home_orig(request):
     quotes = Quote.objects.all()
 #    quotes = quotes[0:2]
     return render_to_response(
