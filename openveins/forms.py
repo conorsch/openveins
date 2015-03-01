cd9e6f82326e053610321cb1f21b7a9c994608fa
Conor Schaefer <conor.schaefer@gmail.com>
2015-02-28 20:45:36 -0500

    prettier creation form

--- openveins/forms.py
+++ openveins/forms.py
@@ -8,6 +8,12 @@ class QuoteForm(forms.ModelForm):
         fields = ['text', 'author', 'source', 'year', 'editorial']
         widgets = {
             'author': forms.TextInput(attrs={'placeholder': 'e.g. Voltaire'}),
-            'text': forms.Textarea(attrs={'placeholder': 'In the beginning...'}),
-            'editorial': forms.Textarea(attrs={'placeholder': 'e.g. lol'}),
+            'text': forms.Textarea(attrs={
+                'placeholder': 'In the beginning...',
+                'rows': 4,
+                }),
+            'editorial': forms.Textarea(attrs={
+                'placeholder': 'e.g. lol',
+                'rows': 4,
+                }),
         }
